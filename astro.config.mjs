d57d8dc9ba857d1f3a2be172724860af125fdbdb
shuhui33333 <Shuhuiq77@gmail.com>
2025-12-20 21:40:01 +1100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
-// @ts-check
+// astro.config.mjs
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import cloudflare from "@astrojs/cloudflare";
 
-// https://astro.build/config
 export default defineConfig({
-  output: "server",          // ⭐⭐⭐ 关键：开启 Server 模式
-  adapter: cloudflare(),     // ⭐ Cloudflare Pages / Workers
-  integrations: [mdx()],     //
+  output: "server",          // ✅ 关键：API 才能 POST
+  adapter: cloudflare(),     // ✅ 关键：Cloudflare Pages Functions
+  integrations: [mdx()],
 });
